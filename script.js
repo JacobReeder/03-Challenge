71bec486c3384fc9617a4d15f7f974c30eb81c70
JacobReeder <jacob.reeder1@gmail.com>
2022-02-27 20:14:07 -0800

    Challenge3

---
+++ script.js
@@ -0,0 +1,39 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var cardbody = document.querySelector("#password");
+
+
+ 
+
+// Write password to the #password input
+function writePassword () {
+  var password = generatePassword("#generate"); 
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+ 
+  console.log(writePassword)
+  
+}
+
+function promptWindows (){
+window.prompt ("Welcome to the Password Generator")
+}
+
+  var generatePassword = function() {
+    var length = 20,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        pass = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+        
+        console.log(generatePassword)
+
+      }
+    return pass;
+}
+  
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+cardbody.addEventListener("click", generatePassword);
+
+
